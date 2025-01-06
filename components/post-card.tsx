c2c0f70e2f916a1c905d5c656c810b7a9b7d7a22
Gaëtan H <me@gaetanhus.fr>
2025-01-06 14:43:36 +0100

    refactor(post-card.tsx): simplify Verified component import and usage for better readability and maintainability

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -6,7 +6,8 @@ import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { Bookmark, Heart, MessageCircle, Repeat, Share, Verified } from "lucide-react";
+import { Bookmark, Heart, MessageCircle, Repeat, Share } from "lucide-react";
+import { Verified } from "./verified";
 
 export const PostCard: Component<PostProps> = ({
   id, author, timestamp, content, replies, reposts, likes, isLiked: isLikedDefault, isReposted: isRepostedDefault, isBookmarked: isBookmarkedDefault
@@ -41,7 +42,7 @@ export const PostCard: Component<PostProps> = ({
           <div className="flex flex-col justify-center gap-0.5">
             <p className="flex items-center gap-1 font-medium">
               {author.name}
-              {author.isVerified && <Verified className="h-4 w-4 text-blue-950 fill-blue-400" />}
+              {author.isVerified && <Verified />}
             </p>
             <span className="-mt-1 font-normal text-gray-500">{author.handle}&nbsp;&bull;&nbsp;{timestamp}</span>
           </div>
