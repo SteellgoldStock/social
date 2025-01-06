1554f8f7bbdb65bcdd5eb5351712bc7093967008
Gaëtan H <me@gaetanhus.fr>
2025-01-06 13:57:57 +0100

    feat(page.tsx): refactor Home component to display tweets using PostCard component for better modularity and readability feat(post-card.tsx): create PostCard component to encapsulate tweet display logic and interactions, improving code organization and reusability

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,47 @@
-import Image from "next/image";
+import { PostCard } from "@/components/post-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+import { PostProps } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { Bookmark, Heart, MessageCircle, Repeat, Share } from "lucide-react";
 
-export default function Home() {
+const tweets: PostProps[] = [
+  {
+    id: "72b9f8bc-2d74-4290-bcf1-f97815477a38",
+    author: { name: 'John Doe', handle: '@johndoe', avatar: '/avatars/john-doe.jpg' },
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor non doloribus eligendi numquam asperiores fugit assumenda animi. Quo sint fugiat quaerat, voluptas aut, unde, dicta a recusandae nisi veniam quidem.',
+    timestamp: '2m',
+    replies: [],
+
+    likes: 35,
+    reposts: 12,
+
+    isBookmarked: false,
+    isLiked: false,
+    isReposted: false,
+  },
+  {
+    id: "2db6e666-a1d6-474a-b4fa-3ef3cd93d5bb",
+    author: { name: 'Jane Doe', handle: '@janedoe', avatar: '/avatars/jane-doe.jpg' },
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptates tenetur atque alias at, deserunt, ipsum, nesciunt distinctio quod commodi optio? Vel nesciunt perspiciatis ea, at laborum delectus doloremque consectetur cupiditate?",
+    timestamp: '5m',
+    replies: [],
+    likes: 23,
+    reposts: 5,
+    isBookmarked: false,
+    isLiked: false,
+    isReposted: false,
+  }
+]
+
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="space-y-4 p-4">
+      {tweets.map((tweet) => <PostCard key={tweet.id} {...tweet} />)}
     </div>
   );
 }
+
+export default Home;
---
+++ components/post-card.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Component, PostProps } from "@/lib/types";
+import { ReactElement, useState } from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { Bookmark, Heart, MessageCircle, Repeat, Share } from "lucide-react";
+
+export const PostCard: Component<PostProps> = ({
+  id, author, timestamp, content, replies, reposts, likes, isLiked: isLikedDefault, isReposted: isRepostedDefault, isBookmarked: isBookmarkedDefault
+}): ReactElement => {
+  const [likesCount, setLikesCount] = useState(likes);
+  const [repostsCount, setRepostsCount] = useState(reposts);
+
+  const [isLiked, setIsLiked] = useState(isLikedDefault);
+  const [isReposted, setIsReposted] = useState(isRepostedDefault);
+  const [isBookmarked, setIsBookmarked] = useState(isBookmarkedDefault);
+
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+    setLikesCount(isLiked ? likesCount - 1 : likesCount + 1);
+  }
+
+  const toggleRepost = () => {
+    setIsReposted(!isReposted);
+    setRepostsCount(isReposted ? repostsCount - 1 : repostsCount + 1);
+  }
+
+  return ( 
+    <Card key={id} className={cn(
+      "bg-[#F9FAFB] dark:bg-[#1A1A1A]"
+    )}>
+      <CardHeader>
+        <div className="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src={author.avatar} alt={author.name} />
+            <AvatarFallback>{author.name.split(' ').map(n => n[0]).join('')}</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col justify-center gap-0.5">
+            <p className="font-medium">
+              {author.name}
+            </p>
+            <span className="-mt-1 font-normal text-gray-500">{author.handle}&nbsp;&bull;&nbsp;{timestamp}</span>
+          </div>
+        </div>
+      </CardHeader>
+
+      <CardContent className="-mt-2">
+        <p>{content}</p>
+      </CardContent>
+
+      <CardFooter>
+        <div className="flex justify-between w-full">
+          <Button variant="ghost" size="sm">
+            <MessageCircle className="h-4 w-4" />
+            {replies.length}
+          </Button>
+
+          <Button variant="ghost" size="sm" onClick={() => toggleLike()}>
+            <Heart className={cn("h-4 w-4", {
+              "fill-red-500 text-red-500": isLiked
+            })} />
+            {likesCount}
+          </Button>
+
+          <Button variant="ghost" size="sm" onClick={() => toggleRepost()}>
+            <Repeat className={cn("h-4 w-4", {
+              "fill-green-500 text-green-500": isReposted
+            })} />
+            {repostsCount}
+          </Button>
+
+          <Button variant="ghost" size="sm" onClick={() => setIsBookmarked(!isBookmarked)}>
+            <Bookmark className={cn("h-4 w-4", {
+              "fill-blue-500 text-blue-500": isBookmarked
+            })} />
+          </Button>
+
+          <Button variant="ghost" size="sm">
+            <Share className="h-4 w-4" />
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
