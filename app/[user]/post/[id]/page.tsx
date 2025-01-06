9896ab4e8b6437526947a98721ef8dc6aea85e28
Gaëtan H <gaetanhus@gmail.com>
2025-01-06 20:50:15 +0100

    feat(page.tsx): enhance post page layout and reply functionality for better user experience style(page.tsx): adjust spacing and structure of components for improved readability and usability

--- app/[user]/post/[id]/page.tsx
+++ app/[user]/post/[id]/page.tsx
@@ -14,47 +14,51 @@ const User = (): ReactElement => {
   const [reply, setReply] = useState("")
 
   const params = useParams<{ user: string, id: string }>()
+
   const user = users.find((u) => u.handle.toLowerCase() === params.user.toLowerCase())
   if (!user) return <div>User not found</div>
+
   const post = findPost(params.id, posts);
   if (!post) return <div>Post not found</div>
 
   return (
-    <section className="container mx-auto px-4">
+    <section className="container mx-auto px-4 py-8">
       <div className="flex flex-col lg:flex-row gap-4">
-        <Card className={cn(
-          "w-full lg:w-2/5 flex flex-col gap-4",
-          "bg-[#F9FAFB] dark:bg-[#1A1A1A] border-b border-[#e1e8ed] dark:border-[#343536] p-4",
-          "sticky top-0 z-50"
-        )}>
-          <PostCard {...post} />
-
+        <div className="w-full lg:w-2/5 lg:sticky lg:top-8 lg:self-start">
           <Card className={cn(
-            "bg-[#F9FAFB] dark:bg-[#1A1A1A]"
+            "flex flex-col gap-4",
+            "bg-[#F9FAFB] dark:bg-[#1A1A1A] border-b border-[#e1e8ed] dark:border-[#343536] p-4"
           )}>
-            <CardContent className="mt-5">
-              <XTextarea
-                placeholder="Répondre à ce post..."
-                onChange={(value: string) => setReply(value)}
-              />
-            </CardContent>
+            <PostCard {...post} />
 
-            <CardFooter>
-              <div className="flex flex-row justify-end w-full items-center gap-3">
-                <div className={cn("text-sm hidden", {
-                  "block": reply.length !== 0,
-                  "text-neutral-500 dark:text-neutral-400": reply.length < MAX_POST_LENGTH - 20,
-                  "text-yellow-500 dark:text-yellow-400": reply.length >= MAX_POST_LENGTH - 20 && reply.length < MAX_POST_LENGTH - 10,
-                  "text-red-500 dark:text-red-400": reply.length >= MAX_POST_LENGTH - 10,
-                })}>
-                  {reply.length}/{MAX_POST_LENGTH}
+            <Card className={cn(
+              "bg-[#F9FAFB] dark:bg-[#1A1A1A]"
+            )}>
+              <CardContent className="mt-5">
+                <XTextarea
+                  placeholder="Répondre à ce post..."
+                  onChange={(value: string) => setReply(value)}
+                />
+              </CardContent>
+
+              <CardFooter>
+                <div className="flex flex-row justify-end w-full items-center gap-3">
+                  <div className={cn("text-sm hidden", {
+                    "block": reply.length !== 0,
+                    "text-neutral-500 dark:text-neutral-400": reply.length < MAX_POST_LENGTH - 20,
+                    "text-yellow-500 dark:text-yellow-400": reply.length >= MAX_POST_LENGTH - 20 && reply.length < MAX_POST_LENGTH - 10,
+                    "text-red-500 dark:text-red-400": reply.length >= MAX_POST_LENGTH - 10,
+                  })}>
+                    {reply.length}/{MAX_POST_LENGTH}
+                  </div>
+
+                  <Button variant="default">Envoyer</Button>
                 </div>
-
-                <Button variant="default">Envoyer</Button>
-              </div>
-            </CardFooter>
+              </CardFooter>
+            </Card>
           </Card>
-        </Card>
+        </div>
+        
         <div className="w-full lg:w-3/5 flex flex-col gap-4">
           {post.replies.length ? post.replies.map((reply) => (
             <PostCard key={reply.id} {...reply} />
