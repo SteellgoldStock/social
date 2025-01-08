5fe122cb02e53753568a12a9e7846ea93bf13c16
Gaëtan H <me@gaetanhus.fr>
2025-01-08 15:45:51 +0100

    feat(x-textarea): add customizable textarea component with auto-resizing and max length validation

---
+++ components/ui/x-textarea.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { MAX_POST_LENGTH } from "@/lib/consts"
+import { Component } from "@/lib/types"
+import React, { useState, useRef, useEffect } from "react"
+
+type TextareaProps = {
+  onChange?: (value: string) => void
+  placeholder?: string
+}
+
+export const XTextarea: Component<TextareaProps> = ({ onChange, placeholder }) => {
+  const [content, setContent] = useState("")
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto"
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
+    }
+  }, [content])
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (MAX_POST_LENGTH && e.target.value.length > MAX_POST_LENGTH) return
+
+    const newValue = e.target.value
+    setContent(newValue)
+    if (onChange) onChange(newValue)
+  }
+
+  return (
+    <textarea
+      ref={textareaRef}
+      value={content}
+      onChange={handleChange}
+      placeholder={placeholder}
+      className="w-full resize-none overflow-hidden bg-transparent text-sm focus:outline-none"
+      maxLength={MAX_POST_LENGTH}
+      rows={1}
+    />
+  )
+}
+
