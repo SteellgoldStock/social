f819baf4f50e93e56e4a951c5a356d1b699cf30c
Gaëtan H <me@gaetanhus.fr>
2025-01-08 16:01:52 +0100

    fix(auth.ts): remove unnecessary exclamation mark from GOOGLE_CLIENT_SECRET to ensure proper handling of environment variable feat(utils.ts): add fixName function to sanitize input names by removing non-alphanumeric characters and converting to lowercase

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs))
 }
+
+export const fixName = (name: string): string => {
+  return name.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+}
