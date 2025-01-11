8c3918edf08649eeac29048d67dc287e7054d4d7
Gaëtan H <gaetanhus@gmail.com>
2025-01-11 20:44:18 +0100

    feat(sidebar): remove unused TypeIcon import and add number badge to NavMain component

--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { TypeIcon, LucideIcon } from "lucide-react"
+import { LucideIcon } from "lucide-react"
 
 import {
   SidebarGroup,
@@ -10,13 +10,16 @@ import {
 } from "@/components/ui/sidebar"
 import Link from "next/link"
 import { Component } from "@/lib/types"
+import { cn } from "@/lib/utils"
 
 type NavMainProps = {
   items: {
     title: string
     url: string
     icon: LucideIcon
+    numberBadge?: number;
   }[]
+
 }
 
 export const NavMain: Component<NavMainProps> = ({ items }) => {
@@ -27,8 +30,15 @@ export const NavMain: Component<NavMainProps> = ({ items }) => {
           <SidebarMenuItem key={item.title}>
             <SidebarMenuButton asChild tooltip={item.title}>
               <Link href={item.url} prefetch passHref>
-                <item.icon />
+                <item.icon strokeWidth={2.5} />
                 <span>{item.title}</span>
+                {item.numberBadge ? (
+                  <span className={cn(
+                    "absolute right-2 flex items-center justify-center w-5 h-5 text-xs font-semibold text-primary-foreground bg-primary rounded-full"
+                  )}>
+                    {item.numberBadge > 99 ? "99+" : item.numberBadge}
+                  </span>
+                ) : null}
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
