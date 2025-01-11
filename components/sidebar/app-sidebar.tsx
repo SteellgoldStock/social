38c0cee77d59c7a0a7f071df87037ab5d6e765ee
Gaëtan H <gaetanhus@gmail.com>
2025-01-11 21:13:27 +0100

    feat(sidebar): implement internationalization for sidebar text

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -9,12 +9,14 @@ import { useTheme } from "next-themes"
 import { NavMain } from "./nav-main"
 import { NavUser } from "./nav-user"
 import { LanguageSelector } from "../language-selector"
+import { useTranslations } from "next-intl"
 
 export const AppSidebar: Component<ComponentProps<typeof Sidebar> & {
   notifications?: number
 }> = ({ ...props }) => {
   const { data: session } = useSession();
   const [notifications, setNotifications] = useState(0);
+  const t = useTranslations("Sidebar");
 
   const { theme, setTheme } = useTheme();
 
@@ -75,7 +77,9 @@ export const AppSidebar: Component<ComponentProps<typeof Sidebar> & {
           <SidebarMenuButton onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span>Switch Theme</span>
+            <span>
+              {t("SwitchTheme")}
+            </span>
           </SidebarMenuButton>
         </SidebarMenu>
 
--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -11,6 +11,7 @@ import {
 import Link from "next/link"
 import { Component } from "@/lib/types"
 import { cn } from "@/lib/utils"
+import { useTranslations } from "next-intl"
 
 type NavMainProps = {
   items: {
@@ -23,6 +24,8 @@ type NavMainProps = {
 }
 
 export const NavMain: Component<NavMainProps> = ({ items }) => {
+  const t = useTranslations("Sidebar");
+
   return (
     <SidebarGroup>
       <SidebarMenu>
@@ -31,7 +34,7 @@ export const NavMain: Component<NavMainProps> = ({ items }) => {
             <SidebarMenuButton asChild tooltip={item.title}>
               <Link href={item.url} prefetch passHref>
                 <item.icon strokeWidth={2.5} />
-                <span>{item.title}</span>
+                <span>{t(item.title)}</span>
                 {item.numberBadge ? (
                   <span className={cn(
                     "absolute right-2 flex items-center justify-center w-5 h-5 text-xs font-semibold text-primary-foreground bg-primary rounded-full"
