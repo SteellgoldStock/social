f947906e62ed6d26370088786a9b56473ae75ae3
Gaëtan H <me@gaetanhus.fr>
2025-01-13 10:45:34 +0100

    feat(new-post/page.tsx): add markdown support information and link to guide to enhance user experience style(new-post/page.tsx): adjust CardFooter layout for better alignment of elements

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { XTextarea } from "@/components/ui/x-textarea";
 import { useCreatePost } from "@/lib/actions/posts/posts.actions";
+import Link from "next/link";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -35,12 +36,20 @@ const NewPostPage = () => {
           <XTextarea placeholder="What's on your mind?" onChange={setContent} />
         </CardContent>
 
-        <CardFooter className="justify-end">
+        <CardFooter className="flex items-center justify-between">
+          <p className="text-xs text-gray-500 mb-2">
+            Markdown is supported.* <Link href="https://www.markdownguide.org/basic-syntax/" target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">Learn more</Link>
+          </p>
+
           <Button variant="default" size="sm" onClick={handlePost}>
             Post
           </Button>
         </CardFooter>
       </Card>
+
+      <div className="text-xs text-gray-500 mt-2">
+        * Only basic markdown, such as bold, italic, underline, and links are supported.
+      </div>
     </div>
   );
 }
